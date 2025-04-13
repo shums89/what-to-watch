814c18850671b9a3620036a076062b2dde39d665
Aleksandr Shumatbaev <sh889@ya.ru>
2025-04-13 12:47:40 +0500

    feat: add pages

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,7 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
-}
+import MainScreen from '../../pages/main/main';
+
+const App = (): JSX.Element => (
+  <MainScreen />
+);
 
 export default App;
