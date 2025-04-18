00855791282d879e8efb21b18c3783d47ac8413b
Aleksandr Shumatbaev <sh889@ya.ru>
2025-04-18 11:19:33 +0500

    feat: render review page

--- project/src/pages/review-screen/review-screen.tsx
+++ project/src/pages/review-screen/review-screen.tsx
@@ -1,88 +1,105 @@
 import { Helmet } from 'react-helmet-async';
 import HeaderUserBlock from '../../components/header-user-block/header-user-block';
 import Logo from '../../components/logo/logo';
+import { Link, useParams } from 'react-router-dom';
+import { Film } from '../../types/film';
+import { films } from '../../mocks/films';
+import NotFoundScreen from '../not-found-screen/not-found-screen';
+import { AppRoute } from '../../const';
 
-const ReviewScreen = (): JSX.Element => (
-  <section className="film-card film-card--full">
-    <Helmet>
-      <title>WTW. Review</title>
-    </Helmet>
-    <div className="film-card__header">
-      <div className="film-card__bg">
-        <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel" />
-      </div>
+const ReviewScreen = (): JSX.Element => {
+  const params = useParams();
+  const paramsId = params.id || 0;
+  const id: number = Number.isInteger(+paramsId) ? +paramsId : 0;
+  const film: Film = films.filter((el) => el.id === +id)[0];
 
-      <h1 className="visually-hidden">WTW</h1>
+  if (!film) {
+    return <NotFoundScreen />;
+  }
 
-      <header className="page-header">
-        <Logo />
+  const { name, posterImage, backgroundImage, backgroundColor } = film;
 
-        <nav className="breadcrumbs">
-          <ul className="breadcrumbs__list">
-            <li className="breadcrumbs__item">
-              <a href="film-page.html" className="breadcrumbs__link">The Grand Budapest Hotel</a>
-            </li>
-            <li className="breadcrumbs__item">
-              <a className="breadcrumbs__link">Add review</a>
-            </li>
-          </ul>
-        </nav>
-
-        <HeaderUserBlock />
-      </header>
-
-      <div className="film-card__poster film-card__poster--small">
-        <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218" height="327" />
-      </div>
-    </div>
-
-    <div className="add-review">
-      <form action="#" className="add-review__form">
-        <div className="rating">
-          <div className="rating__stars">
-            <input className="rating__input" id="star-10" type="radio" name="rating" value="10" />
-            <label className="rating__label" htmlFor="star-10">Rating 10</label>
-
-            <input className="rating__input" id="star-9" type="radio" name="rating" value="9" />
-            <label className="rating__label" htmlFor="star-9">Rating 9</label>
-
-            <input className="rating__input" id="star-8" type="radio" name="rating" value="8" checked />
-            <label className="rating__label" htmlFor="star-8">Rating 8</label>
-
-            <input className="rating__input" id="star-7" type="radio" name="rating" value="7" />
-            <label className="rating__label" htmlFor="star-7">Rating 7</label>
-
-            <input className="rating__input" id="star-6" type="radio" name="rating" value="6" />
-            <label className="rating__label" htmlFor="star-6">Rating 6</label>
-
-            <input className="rating__input" id="star-5" type="radio" name="rating" value="5" />
-            <label className="rating__label" htmlFor="star-5">Rating 5</label>
-
-            <input className="rating__input" id="star-4" type="radio" name="rating" value="4" />
-            <label className="rating__label" htmlFor="star-4">Rating 4</label>
-
-            <input className="rating__input" id="star-3" type="radio" name="rating" value="3" />
-            <label className="rating__label" htmlFor="star-3">Rating 3</label>
-
-            <input className="rating__input" id="star-2" type="radio" name="rating" value="2" />
-            <label className="rating__label" htmlFor="star-2">Rating 2</label>
-
-            <input className="rating__input" id="star-1" type="radio" name="rating" value="1" />
-            <label className="rating__label" htmlFor="star-1">Rating 1</label>
-          </div>
+  return (
+    <section className="film-card film-card--full" style={{ backgroundColor: backgroundColor }}>
+      <Helmet>
+        <title>WTW. Review</title>
+      </Helmet>
+      <div className="film-card__header">
+        <div className="film-card__bg">
+          <img src={backgroundImage} alt={name} />
         </div>
 
-        <div className="add-review__text">
-          <textarea className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text"></textarea>
-          <div className="add-review__submit">
-            <button className="add-review__btn" type="submit">Post</button>
+        <h1 className="visually-hidden">WTW</h1>
+
+        <header className="page-header">
+          <Logo />
+
+          <nav className="breadcrumbs">
+            <ul className="breadcrumbs__list">
+              <li className="breadcrumbs__item">
+                <Link to={`${AppRoute.Films}/${id}`} className="breadcrumbs__link">{name}</Link>
+              </li>
+              <li className="breadcrumbs__item">
+                <a className="breadcrumbs__link">Add review</a>
+              </li>
+            </ul>
+          </nav>
+
+          <HeaderUserBlock />
+        </header>
+
+        <div className="film-card__poster film-card__poster--small">
+          <img src={posterImage} alt={`${name} poster`} width="218" height="327" />
+        </div>
+      </div>
+
+      <div className="add-review">
+        <form action="#" className="add-review__form">
+          <div className="rating">
+            <div className="rating__stars">
+              <input className="rating__input" id="star-10" type="radio" name="rating" value="10" />
+              <label className="rating__label" htmlFor="star-10">Rating 10</label>
+
+              <input className="rating__input" id="star-9" type="radio" name="rating" value="9" />
+              <label className="rating__label" htmlFor="star-9">Rating 9</label>
+
+              <input className="rating__input" id="star-8" type="radio" name="rating" value="8" checked />
+              <label className="rating__label" htmlFor="star-8">Rating 8</label>
+
+              <input className="rating__input" id="star-7" type="radio" name="rating" value="7" />
+              <label className="rating__label" htmlFor="star-7">Rating 7</label>
+
+              <input className="rating__input" id="star-6" type="radio" name="rating" value="6" />
+              <label className="rating__label" htmlFor="star-6">Rating 6</label>
+
+              <input className="rating__input" id="star-5" type="radio" name="rating" value="5" />
+              <label className="rating__label" htmlFor="star-5">Rating 5</label>
+
+              <input className="rating__input" id="star-4" type="radio" name="rating" value="4" />
+              <label className="rating__label" htmlFor="star-4">Rating 4</label>
+
+              <input className="rating__input" id="star-3" type="radio" name="rating" value="3" />
+              <label className="rating__label" htmlFor="star-3">Rating 3</label>
+
+              <input className="rating__input" id="star-2" type="radio" name="rating" value="2" />
+              <label className="rating__label" htmlFor="star-2">Rating 2</label>
+
+              <input className="rating__input" id="star-1" type="radio" name="rating" value="1" />
+              <label className="rating__label" htmlFor="star-1">Rating 1</label>
+            </div>
           </div>
 
-        </div>
-      </form>
-    </div>
+          <div className="add-review__text">
+            <textarea className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text"></textarea>
+            <div className="add-review__submit">
+              <button className="add-review__btn" type="submit">Post</button>
+            </div>
+          </div>
+        </form>
+      </div>
 
-  </section>
-);
+    </section>
+  );
+};
 
 export default ReviewScreen;
